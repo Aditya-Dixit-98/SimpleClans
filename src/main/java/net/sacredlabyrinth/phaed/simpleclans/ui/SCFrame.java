94380eaa76d6f9d3daa0ae857a733a6d55709498
RoinujNosde <edsonpassosjr@outlook.com>
2021-12-17 18:25:10 -0300

    Adds getConfig and readConfig to SCFrame

--- src/main/java/net/sacredlabyrinth/phaed/simpleclans/ui/SCFrame.java
+++ src/main/java/net/sacredlabyrinth/phaed/simpleclans/ui/SCFrame.java
@@ -1,11 +1,18 @@
 package net.sacredlabyrinth.phaed.simpleclans.ui;
 
+import java.io.File;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+
+import net.sacredlabyrinth.phaed.simpleclans.SimpleClans;
 
 /**
  * 
@@ -17,10 +24,12 @@ public abstract class SCFrame {
 	private final SCFrame parent;
 	private final Player viewer;
 	private final Set<SCComponent> components = ConcurrentHashMap.newKeySet();
+	private final FileConfiguration config;
 	
 	public SCFrame(@Nullable SCFrame parent, @NotNull Player viewer) {
 		this.parent = parent;
 		this.viewer = viewer;
+		this.config = readConfig();
 	}
 
 	@NotNull
@@ -63,6 +72,10 @@ public abstract class SCFrame {
 		return components;
 	}
 
+	public FileConfiguration getConfig() {
+		return config;
+	}
+
 	@Override
 	public boolean equals(Object other) {
 		if (other instanceof SCFrame) {
@@ -77,6 +90,23 @@ public abstract class SCFrame {
 	@Override
 	public int hashCode() {
 		return getTitle().hashCode() + Integer.hashCode(getSize()) + getComponents().hashCode();
+	}
+
+	private YamlConfiguration readConfig() {
+		SimpleClans plugin = SimpleClans.getInstance();
+		File externalFile = new File(plugin.getDataFolder(), getConfigPath());
+		InputStream resource = plugin.getResource(getConfigPath());
+		YamlConfiguration config = YamlConfiguration.loadConfiguration(externalFile);
+		if (resource != null) {
+			YamlConfiguration defaults = YamlConfiguration.loadConfiguration(new InputStreamReader(resource));
+			config.setDefaults(defaults);
+		}
+		return config;
+	}
+
+	private String getConfigPath() {
+		return getClass().getName().replace("net.sacredlabyrinth.phaed.simpleclans.ui.", "")
+				.replace(".", File.pathSeparator);
 	}
 
 }
